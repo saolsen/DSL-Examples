39778a1a55275997775a6a5c923091179b48e3bb
Stephen Olsen <steveo@hack-net.org>
2012-02-03 18:25:34 -0600

    examples

--- src/DSL_Examples/core.clj
+++ src/DSL_Examples/core.clj
@@ -1,33 +1,26 @@
 (ns DSL-Examples.core)
 
-;; So mathematically a DFA is defined as a 5 tuple (1 2 3 4 5)
-;; A finite set of states
-;; The Alphabet
-;; A transition function
-;; A start state
-;; and a set of accept states.
-
-(def states #{:A :B :C :D})
+(def states #{:a :b :c :d})
 (def alphabet #{\0 \1})
 (defn transition
   [state char]
-  (if (= state :A)
+  (if (= state :a)
     (if (= char \0)
-      :A
-      :B)
-    (if (= state :B)
+      :a
+      :b)
+    (if (= state :b)
       (if (= char \0)
-        :B
-        :C)
-      (if (= state :C)
+        :b
+        :d)
+      (if (= state :c)
         (if (= char \0)
-          :D
-          :A)
+          :d
+          :a)
         (if (= char \0)
-          :D
-          :D)))))
-(def start-state :A)
-(def accept-states #{:D})
+          :d
+          :d)))))
+(def start-state :a)
+(def accept-states #{:d})
 
 (defn DFA1
   "Recognizes strings of 1's and 0's with 010 as a substring"
@@ -35,3 +28,35 @@
   (let [s start-state
         f (reduce #(transition %1 %2) s input)]
     (contains? accept-states f)))
+
+(defn MakeDFA
+  "Makes a DFA from the components"
+  [states alphabet tran start accepts]
+  (fn [input] (let [s start
+                    f (reduce #(tran %1 %2) s input)]
+                (contains? accepts f))))
+
+(def state-lst [:a :b :c :d])
+(def alphabet-lst [\0 \1])
+(def matrix [[:b :a]
+             [:b :c]
+             [:d :a]
+             [:d :d]])
+
+(defn MakeTransition
+  [states alphabet matrix]
+  (let [mapper (fn [x y] {x y})
+        inner-maps (map #(reduce merge (map mapper alphabet %)) matrix)
+        outer-maps (map mapper states inner-maps)
+        lookup (reduce merge outer-maps)]
+    (fn [state char]
+      (get (get lookup state) char))))
+
+(defn MakeDFA2
+  "Create a DFA from it's components"
+  [states alphabet matrix start accepts]
+  (let [trans (MakeTransition states alphabet matrix)]
+    (fn [input]
+      (let [s start
+                          f (reduce #(trans %1 %2) s input)]
+                      (contains? accepts f)))))
